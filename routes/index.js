b61a84f0bdad179d607fb36a54f87e3ebe360154
Will Darkins <willdarkins@gmail.com>
2021-11-03 21:14:36 -0700

    index file collecting api endpoints to prefix is created

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+// Collecting  packaged group of API endpoints and prefixing them with the path /api
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
+
+module.exports = router;
