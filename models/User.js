bc827dba42b341e1d20add28aec250a538f4337a
Will Darkins <willdarkins@gmail.com>
2021-11-03 23:13:28 -0700

    added bcrypt hash function to user password and tested

--- models/User.js
+++ models/User.js
@@ -4,51 +4,50 @@ const sequelize = require('../config/connection');
 //Imported bcrypt dependency
 const bcrypt = require('bcrypt');
 
-class User extends Model {}
+class User extends Model { }
 
 // Initialize model's data and configuration columns to set rules
 User.init(
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-          isEmail: true
-        }
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [8]
-        }
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
       }
     },
-    {
-      hooks: {
-          // set up beforeCreate lifecycle "hook" functionality
-  beforeCreate(userData) {
-    return bcrypt.hash(userData.password, 10).then(newUserData => {
-      return newUserData
-    });
-  }
-      },
-      sequelize,
-      timestamps: false,
-      freezeTableName: true,
-      underscored: true,
-      modelName: 'user'
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8]
+      }
     }
-  );
+  },
+  {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+  }
+);
 module.exports = User;
