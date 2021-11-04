456fc41d5696e002b5471914d6cadd7b0817e956
Will Darkins <willdarkins@gmail.com>
2021-11-03 20:26:05 -0700

    .env dependencies, file and load declaration added to project

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,8 @@
-// Importing  Sequelize constructor from library
+// Importing  base Sequelize class to create a new connection to the database
 const Sequelize = require('sequelize');
+
+//Loading enviornment variables
+require('dotenv').config();
 
 // Connection to  database
 const sequelize = new Sequelize('darkins_tech_blog_db', 'username', 'password', {
