b834b39c6a03cd29046ab799e0886aeedf2d4db8
Will Darkins <willdarkins@gmail.com>
2021-11-03 21:04:20 -0700

    Rough start to POST route updating users by id

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -5,46 +5,66 @@ const { User } = require('../../models');
 // GET /api/users - Get all users
 router.get('/', (req, res) => {
     User.findAll()
-      .then(UserData => res.json(UserData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+        .then(UserData => res.json(UserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
-  // GET /api/users/1 - Get one user by id
+// GET /api/users/1 - Get one user by id
 router.get('/:id', (req, res) => {
     User.findOne({
-      where: {
-        id: req.params.id
-      }
-    })
-      .then(UserData => {
-        if (!UserData) {
-          res.status(404).json({ message: 'No user found with this id' });
-          return;
+        where: {
+            id: req.params.id
         }
-        res.json(UserData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+    })
+        .then(UserData => {
+            if (!UserData) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(UserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
-  // POST /api/users - Create a user
+// POST /api/users - Create a user
 router.post('/', (req, res) => {
     User.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
     })
-      .then(dbUserData => res.json(dbUserData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+        .then(UserData => res.json(UserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// PUT /api/users/1 - Update user info by id
+router.put('/:id', (req, res) => {
+    User.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData[0]) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 module.exports = router;
 
