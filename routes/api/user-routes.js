d17ca79f921d4bacc2a37e4ab71ce8a5f79c209f
Will Darkins <willdarkins@gmail.com>
2021-11-03 23:46:35 -0700

    bcrypt password verification tested in Insomnia

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -53,22 +53,24 @@ router.post('/', (req, res) => {
 
 // POST /api/login - Login route to verify user identity
 router.post('/login', (req, res) => {
-    User.findOne({
-      where: {
-        email: req.body.email
-      }
-    }).then(dbUserData => {
-      if (!dbUserData) {
-        res.status(400).json({ message: 'No user with that email address!' });
-        return;
-      }
-  
-      // res.json({ user: dbUserData });
-  
-      // Verify user
-  
-    });  
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+    // Verify user
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
   });
+});
 
 // PUT /api/users/1 - Update user info by id
 router.put('/:id', (req, res) => {
