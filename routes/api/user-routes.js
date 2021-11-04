d60a29e1c435af6707092a55ca4d7420fbd11cec
Will Darkins <willdarkins@gmail.com>
2021-11-03 21:00:26 -0700

    Rough start for POST route of creating one user

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -32,5 +32,19 @@ router.get('/:id', (req, res) => {
       });
   });
 
+  // POST /api/users - Create a user
+router.post('/', (req, res) => {
+    User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
 module.exports = router;
 
