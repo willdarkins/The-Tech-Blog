2636889e419a1ec0ae4b73c084cc55c01d681fc7
Will Darkins <willdarkins@gmail.com>
2021-11-03 20:58:02 -0700

    rough start to getting one user by id

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -12,4 +12,25 @@ router.get('/', (req, res) => {
       });
   });
 
+  // GET /api/users/1 - Get one user by id
+router.get('/:id', (req, res) => {
+    User.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(UserData => {
+        if (!UserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(UserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
 module.exports = router;
+
