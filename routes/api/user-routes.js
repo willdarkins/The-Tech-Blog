65103a707633bb245457ca09f3163cf6ab57e37f
Will Darkins <willdarkins@gmail.com>
2021-11-03 23:23:50 -0700

    hashed password functionality added before update PUT route in User model

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,92 +4,93 @@ const { User } = require('../../models');
 
 // GET /api/users - Get all users
 router.get('/', (req, res) => {
-    User.findAll({
-        //Provided  attributes key and instructed the query to exclude the password column
-        // attributes: { exclude: ['password'] }
-      })
-        .then(UserData => res.json(UserData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+  User.findAll({
+    //Provided  attributes key and instructed the query to exclude the password column
+    // attributes: { exclude: ['password'] }
+  })
+    .then(UserData => res.json(UserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // GET /api/users/1 - Get one user by id
 router.get('/:id', (req, res) => {
-    User.findOne({
-        //Provided  attributes key and instructed the query to exclude the password column
-        // attributes: { exclude: ['password'] }
-        where: {
-          id: req.params.id
-        }
-      })
-        .then(UserData => {
-            if (!UserData) {
-                res.status(404).json({ message: 'No user found with this id' });
-                return;
-            }
-            res.json(UserData);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+  User.findOne({
+    //Provided  attributes key and instructed the query to exclude the password column
+    // attributes: { exclude: ['password'] }
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(UserData => {
+      if (!UserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(UserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // POST /api/users - Create a user
 router.post('/', (req, res) => {
-    User.create({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    })
-        .then(UserData => res.json(UserData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  })
+    .then(UserData => res.json(UserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // PUT /api/users/1 - Update user info by id
 router.put('/:id', (req, res) => {
-    User.update(req.body, {
-        where: {
-            id: req.params.id
-        }
+  User.update(req.body, {
+    individualHooks: true,
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData[0]) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
     })
-        .then(dbUserData => {
-            if (!dbUserData[0]) {
-                res.status(404).json({ message: 'No user found with this id' });
-                return;
-            }
-            res.json(dbUserData);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // DELETE /api/users/1- Delete user from database using id
 router.delete('/:id', (req, res) => {
-    User.destroy({
-      where: {
-        id: req.params.id
+  User.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
       }
+      res.json(dbUserData);
     })
-      .then(dbUserData => {
-        if (!dbUserData) {
-          res.status(404).json({ message: 'No user found with this id' });
-          return;
-        }
-        res.json(dbUserData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
 
