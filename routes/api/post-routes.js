3b55009c683332e274e464333caf7b451c0c6e89
Will Darkins <willdarkins@gmail.com>
2021-11-04 23:00:14 -0700

    POST route to recieve all posts via user information coded out with connection to database and tests complete

---
+++ routes/api/post-routes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const { Post, User } = require('../../models');
+
+// get all users associated with posts
+router.get('/', (req, res) => {
+    console.log('======================');
+    Post.findAll({
+        attributes: ['id', 'post_url', 'title', 'created_at'],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
