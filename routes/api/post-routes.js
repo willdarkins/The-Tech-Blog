23379a0c374d57e8c6c7f21bb63e536155d76bfb
Will Darkins <willdarkins@gmail.com>
2021-11-05 00:23:44 -0700

    refactored some code on post.js and post-routes.js

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -77,32 +77,13 @@ router.post('/', (req, res) => {
 // upvote posts
 router.put('/upvote', (req, res) => {
     // custom static method created in models/Post.js
-    Vote.create({
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-    }).then(() => {
-        return Post.findOne({
-            where: {
-                id: req.body.post_id
-            },
-            attributes: [
-                'id',
-                'post_url',
-                'title',
-                'created_at',
-                [
-                    sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
-                    'vote_count'
-                ]
-            ]
-        })
-            .then(dbPostData => res.json(dbPostData))
-            .catch(err => {
-                console.log(err)
-                res.status(400).json(err);
-            })
-    })
-});
+    Post.upvote(req.body, { Vote })
+      .then(updatedPostData => res.json(updatedPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  });
 
 //updating an existing post via title
 router.put('/:id', (req, res) => {
