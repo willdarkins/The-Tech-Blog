a5a8776c0604f9c4261c2abb643eebf45ce08a23
Will Darkins <willdarkins@gmail.com>
2021-11-04 23:03:41 -0700

    GET route to retrieve a single post associated with an id has been coded and tested

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -20,4 +20,31 @@ router.get('/', (req, res) => {
         });
 });
 
+// get one post by query of post id
+router.get('/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: ['id', 'post_url', 'title', 'created_at'],
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
 module.exports = router;
