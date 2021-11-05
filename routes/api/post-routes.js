c1a677c3469a139ba6afb117eaff4d14fe10b86a
Will Darkins <willdarkins@gmail.com>
2021-11-04 23:19:05 -0700

    order property added to GET route to retrieve all posts - which will now be in descending order

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -6,6 +6,7 @@ router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
         attributes: ['id', 'post_url', 'title', 'created_at'],
+        order: [['created_at', 'DESC']],
         include: [
             {
                 model: User,
