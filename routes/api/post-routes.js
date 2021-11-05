3fe88ba3d9afb898affd77b8acd6bbc8792dc02f
Will Darkins <willdarkins@gmail.com>
2021-11-04 23:13:40 -0700

    PUT route that updates a post title has been coded and tested

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -61,4 +61,29 @@ router.get('/:id', (req, res) => {
       });
   });
 
+  //updating an existing post via title
+  router.put('/:id', (req, res) => {
+    Post.update(
+      {
+        title: req.body.title
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
 module.exports = router;
