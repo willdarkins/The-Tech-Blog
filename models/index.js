b663e0811bd65f6920ba70316b9ced12f42ebbb2
Will Darkins <willdarkins@gmail.com>
2021-11-06 19:05:05 -0700

    Framework for comment routes coded and instructed to export with router instance

--- models/index.js
+++ models/index.js
@@ -41,4 +41,20 @@ Post.hasMany(Vote, {
     foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote, Comment};
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
---
+++ routes/api/comment-routes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+
+router.get('/', (req, res) => {
+
+});
+
+router.post('/', (req, res) => {
+
+});
+
+router.delete('/:id', (req, res) => {
+
+});
+
+module.exports = router;
