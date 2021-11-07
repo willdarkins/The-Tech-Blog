1dbaff3b72f0dc89230de96bdbbbae9b5ae6f575
Will Darkins <willdarkins@gmail.com>
2021-11-06 19:01:57 -0700

    Associations written for comment model

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@
 const User = require("./User");
 const Post = require("./Post");
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 //Defining model associations
 User.hasMany(Post, {
@@ -40,4 +41,4 @@ Post.hasMany(Vote, {
     foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote};
+module.exports = { User, Post, Vote, Comment};
