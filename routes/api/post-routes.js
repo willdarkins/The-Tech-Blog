4a85ba488357d9317e01a531a474043dcf7ffe58
Will Darkins <willdarkins@gmail.com>
2021-11-06 20:00:06 -0700

    same logic is tested and works for finding single post with comment

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -52,6 +52,14 @@ router.get('/:id', (req, res) => {
           ],
         include: [
             {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                  model: User,
+                  attributes: ['username']
+                }
+              },
+            {
                 model: User,
                 attributes: ['username']
             }
