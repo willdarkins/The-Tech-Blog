a6a79767e7708e1db3e6b6038ecc949bb2e2e1c8
Will Darkins <willdarkins@gmail.com>
2021-11-06 21:38:57 -0700

    fixed small issue with homepage not rendering on localhost... miscode

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,12 +4,10 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes')
 const commentRoutes = require('./comment-routes')
-const homeRoutes = require('./home-routes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
-router.use('/', homeRoutes);
 
 module.exports = router;
 
