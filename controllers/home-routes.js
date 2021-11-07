d91a47ef1260c81fc34f0f7b75372a89144c083f
Will Darkins <willdarkins@gmail.com>
2021-11-07 13:36:47 -0800

    having some issue with the signup handler, but it seems to have resolved itself

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
+//POST route to populate page with all posts in the database
 router.get('/', (req, res) => {
   Post.findAll({
     attributes: [
@@ -36,6 +37,11 @@ router.get('/', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+//GET route to display login information
+router.get('/login', (req, res) => {
+  res.render('login');
 });
 
 module.exports = router;
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,57 +1,54 @@
-// Login form front handler begining
-async const signupFormHandler = event => {
+async function loginFormHandler(event) {
     event.preventDefault();
-
+  
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (email && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'post',
+        body: JSON.stringify({
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+  
+  async function signupFormHandler(event) {
+    event.preventDefault();
+  
     const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
-
+  
     if (username && email && password) {
-        const response = await fetch('/api/users', {
-            method: 'post',
-            body: JSON.stringify({
-                username,
-                email,
-                password
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        })
-        // check the response status
-        if (response.ok) {
-            console.log('success');
-        } else {
-            alert(response.statusText);
-        }
+      const response = await fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({
+          username,
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
     }
-}
-//Login form front handler end
-
-//Sign up form front handler begining
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
-
-async function loginFormHandler(event) {
-    event.preventDefault();
-
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-
-    if (email && password) {
-        const response = await fetch('/api/users/login', {
-            method: 'post',
-            body: JSON.stringify({
-                email,
-                password
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        });
-        // check the response status
-        if (response.ok) {
-            document.location.replace('/');
-        } else {
-            alert(response.statusText);
-        }
-    }
-}
-
-document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
-//Sign up form front handler end
+  }
+  
+  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+  
+  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+  
