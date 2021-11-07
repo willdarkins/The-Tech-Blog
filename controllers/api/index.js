810b6640a6029e8b12bfd7c8f6b2f352508bbb46
Will Darkins <willdarkins@gmail.com>
2021-11-06 21:36:27 -0700

    home-routes added to controllers directory

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,9 +4,16 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes')
 const commentRoutes = require('./comment-routes')
+const homeRoutes = require('./home-routes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
+router.use('/', homeRoutes);
 
 module.exports = router;
+
+
+
+
+
