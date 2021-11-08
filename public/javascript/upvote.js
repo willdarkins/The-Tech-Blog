349c81113e7224b1ba81ec04fd95f84902f9b82c
Will Darkins <willdarkins@gmail.com>
2021-11-07 19:38:37 -0800

    comments work now

--- public/javascript/upvote.js
+++ public/javascript/upvote.js
@@ -1,8 +1,20 @@
 async function upvoteClickHandler(event) {
     event.preventDefault();
   
-    console.log('button clicked');
-  }
-  
-  document.querySelector('.upvote-btn').addEventListener('click', upvoteClickHandler);
-  
+    const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+    const response = await fetch('/api/posts/upvote', {
+        method: 'PUT',
+        body: JSON.stringify({
+          post_id: id
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      if (response.ok) {
+        document.location.reload();
+      } else {
+        alert(response.statusText);
+      }  }
