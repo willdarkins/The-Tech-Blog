817cf96cce83afa84fd9b8c27176a13250d4f38c
Will Darkins <willdarkins@gmail.com>
2021-11-07 19:28:28 -0800

    upvote button logic added to public file

---
+++ public/javascript/upvote.js
@@ -0,0 +1,8 @@
+async function upvoteClickHandler(event) {
+    event.preventDefault();
+  
+    console.log('button clicked');
+  }
+  
+  document.querySelector('.upvote-btn').addEventListener('click', upvoteClickHandler);
+  
