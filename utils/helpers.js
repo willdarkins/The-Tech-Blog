99ead41a92bb056a52590934eeabcc6b34083f09
Will Darkins <willdarkins@gmail.com>
2021-11-07 22:02:33 -0800

    helpers for shortening domain and pularlizing comment count added

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,25 @@
+// Makes dates more readable for each post
 module.exports = {
     format_date: date => {
-      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
-        date
-      ).getFullYear()}`;
-    }
-  }
+        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
+            date
+        ).getFullYear()}`;
+    },
+//Creates plural option to count each comment
+    format_plural: (word, amount) => {
+        if (amount !== 1) {
+            return `${word}s`;
+        }
+
+        return word;
+    },
+//Formats urls with clean read - Deletes domains
+    format_url: url => {
+        return url
+        .replace('http://', '')
+        .replace('https://', '')
+        .replace('www.', '')
+        .split('/')[0]
+        .split('?')[0];
+      },
+}
