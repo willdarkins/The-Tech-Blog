8ff8a5356ea1908f6c47cd175f58cc0369af20bb
Will Darkins <willdarkins@gmail.com>
2021-11-11 23:40:40 -0800

    final bootstrap styles added to comments and various buttons - still need to get a test post up

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -23,4 +23,4 @@ async function newFormHandler(event) {
     }
   }
   
-  document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+  document.querySelector('#post-submit-btn').addEventListener('submit', newFormHandler);
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -50,4 +50,4 @@ async function signupFormHandler(event) {
 
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
 
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+document.querySelector('#signup-btn').addEventListener('submit', signupFormHandler);
